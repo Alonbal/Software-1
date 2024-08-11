b208d4922a42a10d8830e46229dfab67f219c481
Alonbal <alonbalas7@gmail.com>
2024-08-11 13:08:58 +0300

    Add files via upload

---
+++ hw1-alonbalassiano/src/Assignment1.java
@@ -0,0 +1,10 @@
+
+
+public class Assignment1 {
+	public static void main(String[] args){
+		char chr = args[0].charAt(0);
+		int offset = Integer.valueOf(args[1]);
+		char newChar = (char)(chr + offset);
+		System.out.println("New char is " + newChar + ".");
+	}
+}
